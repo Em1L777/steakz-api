e732908e593ce9b2ab3d0ad93b0b0e0e0633ebdd
Em1L777 <emilfaizullov@gmail.com>
2026-06-07 12:25:25 +0200

    added images and updated size 2

--- src/index.ts
+++ src/index.ts
@@ -39,8 +39,8 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(express.json({ limit: '100mb' }));
-app.use(express.urlencoded({ limit: '100mb', extended: true }));
+app.use(express.json({ limit: '200mb' }));
+app.use(express.urlencoded({ limit: '200mb', extended: true }));
 app.use(requestLogger);
 
 app.use('/uploads', express.static(path.join(__dirname, '../public/uploads')));
